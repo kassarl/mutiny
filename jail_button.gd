d67167598c53fe5691b8bfc3080529cc898a250e
BRYAN <bryanwang105@gmail.com>
2024-11-17 10:58:33 -0800

    Added door locked syncronization to jail door across client and host

--- jail_button.gd
+++ jail_button.gd
@@ -1,26 +1,51 @@
 extends StaticBody3D
 
 @onready var anim_player: AnimationPlayer = $"../AnimationPlayer"
+@export var doorOpen: bool
 
 var prompt
-var isOpen
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	prompt = "Press E to open jail"
-	isOpen = false
+	doorOpen = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+# For clients to request jail door state change
+@rpc("any_peer")
+func request_door_state_change() -> void:
+	# Only server should process the request
+	if multiplayer.is_server():
+		var peer_id = multiplayer.get_remote_sender_id()
+		print("Received door update request from peer: ", peer_id)
+		
+		# Update and sync to all clients
+		sync_door_state.rpc(!doorOpen)  # Toggle the current state
+
+# Syncs host and clients
+@rpc("authority", "call_local")
+func sync_door_state(newState: bool):
+	
+	if doorOpen:
+		anim_player.play('moveDoor')
+	else:
+		anim_player.play_backwards('moveDoor')
+	
+	
+	doorOpen = newState  # Set the state after playing animation
 
 func get_prompt():
 	return prompt
 
 func interact():
-	if isOpen:
-		anim_player.play('moveDoor')
+	if multiplayer.is_server():
+		print("Calling door sync to clients")
+		print("Is door open?")
+		print(doorOpen)
+		print("Syncing to clients:")
+		# Server directly syncs the new state
+		sync_door_state.rpc(!doorOpen)
 	else:
-		anim_player.play_backwards('moveDoor')
-	
-	isOpen = !isOpen
+		# Clients request a state change
+		request_door_state_change.rpc()
