5c1d957dc434a75533b1d20cd50ca2f3f588cc39
BRYAN <bryanwang105@gmail.com>
2024-11-22 20:40:14 -0800

    Fixed pause menu mouse capture minor bug

--- Environment/pause_menu.gd
+++ Environment/pause_menu.gd
@@ -31,11 +31,12 @@ func pause_game() -> void:
 func unpause_game() -> void:
 	game_paused = false
 	visible = false  # Hide the pause menu
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 # Called when the Resume button is pressed
 func _on_resume_pressed() -> void:
 	unpause_game()
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 
 # Called when the Quit button is pressed
 func _on_quit_pressed() -> void:
